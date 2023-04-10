fe1b66b98d75707d06ae8c3a8f4b2fd858044225
vantadam <adem.zaier.9@gmail.com>
2023-04-10 13:45:41 +0100

    React1

---
+++ centre-front/src/Pages/Login.js
@@ -0,0 +1,11 @@
+import "./Login.css"
+function Login(){
+    
+return (
+    <div>
+    Email: <input type="text"></input>
+    Password: <input type="text"></input>
+    </div>
+)
+}
+export default Login
